6e7a53cef1cd48a6cd47c5774d1f3eeb9e0fef42
Duoc Nguyen <nguyenvanduocit@gmail.com>
2018-10-09 16:45:28 +0700

    add created field

--- feed.go
+++ feed.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gorilla/feeds"
+import (
+	"github.com/gorilla/feeds"
+	"time"
+)
 
 func generateFeed(domain string) (*feeds.Feed, error) {
 	client := NewClient(domain)
@@ -31,12 +34,19 @@ func generateFeed(domain string) (*feeds.Feed, error) {
 	}
 
 	for _, post := range posts {
-		feed.Items = append(feed.Items, &feeds.Item{
+		item := &feeds.Item{
 			Title:       post.Title.Rendered,
 			Link:        &feeds.Link{Href: post.GUID.Rendered},
 			Description: post.Excerpt.Rendered,
 			Content:     post.Content.Rendered,
-		})
+		}
+		layout := "2006-01-02T15:04:05"
+		createdTime, err := time.Parse(layout, post.Date)
+		if err != nil {
+			return nil, err
+		}
+		item.Created = createdTime
+		feed.Items = append(feed.Items, item)
 	}
 
 	return feed, nil
